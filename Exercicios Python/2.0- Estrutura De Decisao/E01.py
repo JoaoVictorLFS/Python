3689ff59c4c2f947329db408b07b9e0100e92872
João Victor Luna <Joaolfsantiago@gmail.com>
2022-04-08 21:19:24 -0300

    Python

---
+++ Exercicios Python/2.0- Estrutura De Decisao/E01.py
@@ -0,0 +1,7 @@
+#1 -Faça um Programa que peça dois números e imprima o maior deles.
+numero1 = float(input("Digite um numero: "))
+numero2 = float(input("digite outro numero: "))
+if numero1 > numero2:
+    print(numero1, "É seu maior numero")
+else:
+    print(numero2, "É seu maior numero")
